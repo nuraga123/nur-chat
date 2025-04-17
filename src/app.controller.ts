ab3e7fe6526db0a596c2cb57f5285e47c77cf99d
nuraga123 <sabrabat54@gmail.com>
2025-04-18 00:10:15 +0400

    Redirect /chat

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get, Render, Res } from '@nestjs/common';
+import { Controller, Get, Redirect, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('/')
+  @Redirect('/chat', 301)
+  redirectToChat() {}
 
   @Get('/chat')
   @Render('index')
